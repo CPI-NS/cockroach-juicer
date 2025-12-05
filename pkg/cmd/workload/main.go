6b6ff50bf6220865495bc2a228cccd74c0884025
harris-ahmad <harrisah@buffalo.edu>
2025-12-04 21:58:03 -0500

    workload gen updated

--- pkg/cmd/workload/main.go
+++ pkg/cmd/workload/main.go
@@ -1,4 +1,4 @@
-// Copyright 2018 The Cockroach Authors.
+]// Copyright 2018 The Cockroach Authors.
 //
 // Use of this software is governed by the CockroachDB Software License
 // included in the /LICENSE file.
